2ba701ec4bb01c513f287a1cf8a8744234f6eda9
troylelandshields <troy.leland.shields@gmail.com>
2019-01-08 23:32:46 -0700

    Updates

---
+++ precache-manifest.78cdd986f6f2bc1250380a3ea2cd23eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/daily-journal-ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bdc03da289d00dfaad5d",
+    "url": "/daily-journal-ui/static/js/runtime~main.bdc03da2.js"
+  },
+  {
+    "revision": "0e0e2b4c748b7165b32c",
+    "url": "/daily-journal-ui/static/js/main.0e0e2b4c.chunk.js"
+  },
+  {
+    "revision": "6b4a7f01b422a33e647a",
+    "url": "/daily-journal-ui/static/js/1.6b4a7f01.chunk.js"
+  },
+  {
+    "revision": "0e0e2b4c748b7165b32c",
+    "url": "/daily-journal-ui/static/css/main.6c4a5032.chunk.css"
+  },
+  {
+    "revision": "6b4a7f01b422a33e647a",
+    "url": "/daily-journal-ui/static/css/1.8c3f2fdf.chunk.css"
+  },
+  {
+    "revision": "4bf953a2a9648cc5f3f7940b2379dcb5",
+    "url": "/daily-journal-ui/index.html"
+  }
+];
