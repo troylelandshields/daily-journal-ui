4df704304547e50dfb5555fe3516a4ba1f888d9c
troylelandshields <troy.leland.shields@gmail.com>
2019-01-08 23:25:21 -0700

    commit built assets? sure

---
+++ build/precache-manifest.b271f37c9f91f1aba17e0346552f8c45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e0e2b4c748b7165b32c",
+    "url": "/static/js/main.0e0e2b4c.chunk.js"
+  },
+  {
+    "revision": "6b4a7f01b422a33e647a",
+    "url": "/static/js/1.6b4a7f01.chunk.js"
+  },
+  {
+    "revision": "0e0e2b4c748b7165b32c",
+    "url": "/static/css/main.6c4a5032.chunk.css"
+  },
+  {
+    "revision": "6b4a7f01b422a33e647a",
+    "url": "/static/css/1.8c3f2fdf.chunk.css"
+  },
+  {
+    "revision": "00beffff6c73771edc54800dfb1f6fc9",
+    "url": "/index.html"
+  }
+];
