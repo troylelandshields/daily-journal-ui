238edebb81b0bdcc6379448e43354d703fdfdee3
troylelandshields <troy.leland.shields@gmail.com>
2019-01-16 22:44:53 -0700

    remove trailing slash

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 const config = {
     // apiHost: "http://localhost:3000",
     // stripeKey: "pk_test_Yptgsj6W2rIreWbKt0mUi2R5"
-    apiHost: "https://buffalo-journal.herokuapp.com/",
+    apiHost: "https://buffalo-journal.herokuapp.com",
     stripeKey: "pk_live_doxQ9etmZ8nozRk89VmvoOrZ"
 }
 
