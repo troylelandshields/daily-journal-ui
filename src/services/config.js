dceaa775fdaf280c041a04195d0d714383526ae0
Troy <troy@be-brite.com>
2023-03-30 23:30:12 -0600

    fix config

--- src/services/config.js
+++ src/services/config.js
@@ -1,8 +1,8 @@
 const config = {
-    apiHost: "http://localhost:3000",
-    stripeKey: "pk_test_Yptgsj6W2rIreWbKt0mUi2R5"
-    // apiHost: "https://buffalo-journal.herokuapp.com",
-    // stripeKey: "pk_live_doxQ9etmZ8nozRk89VmvoOrZ"
+    // apiHost: "http://localhost:3000",
+    // stripeKey: "pk_test_Yptgsj6W2rIreWbKt0mUi2R5"
+    apiHost: "https://buffalo-journal.herokuapp.com",
+    stripeKey: "pk_live_doxQ9etmZ8nozRk89VmvoOrZ"
 }
 
 export default config;
